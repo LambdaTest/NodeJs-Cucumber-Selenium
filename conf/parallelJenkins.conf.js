ce0ef4ce545b2289072f17a13055584c8bee4197
hp <ankitp@lambdatest.com>
2019-01-24 22:33:20 +0530

    Sample Tests

--- conf/parallelJenkins.conf.js
+++ conf/parallelJenkins.conf.js
@@ -13,7 +13,7 @@ exports.config = {
 
   commonCapabilities: {
     name: "parallel_test",
-    build: "cucumber-js-LambdaTest-parallel"
+    build: "cucumber-js-LambdaTest-parallel",
     'video': true,
     'network': true,
     'console': true,
